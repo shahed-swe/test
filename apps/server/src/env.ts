f862e284f93a94ba320c06c5b53e6f9622006f77
Md Shahed Talukder <shahedtalukder51@gmail.com>
2025-02-25 11:37:40 +0600

    docker setup with hot reload is done

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -2,7 +2,7 @@ import "dotenv/config.js";
 
 export default function env() {
   return ({
-    PORT: process.env.PORT,
-    POSTGRES_URL: process.env.POSTGRES_URL
+    PORT: process.env.PORT || '3001',
+    POSTGRES_URL: process.env.POSTGRES_URL || 'postgres://postgres:password@localhost/testfb'
   })
 }
